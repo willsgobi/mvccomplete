8f4cc77556d3355b9000b409c4ee90ad9cadb491
willsgobi <william.sgobi@programaconsumer.com.br>
2021-08-29 17:27:46 -0300

    first commit

---
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.App.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public Guid ForncedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "O campo {0} precisa estar entre {2} e {1} caracteres.")]
+        public string Nome { get; set; }
+
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "O campo {0} precisa estar entre {2} e {1} caracteres.")]
+        public string Descricao { get; set; }
+
+        //public IFormFile ImagemUpload { get; set; }
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        public decimal Valor { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        public FornecedorViewModel Fornecedor { get; set; }
+    }
+}
